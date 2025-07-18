ab7d8917ade5bdd38be9f6eee98d04e7054724e3
Wonder <hezhaohui0807@163.com>
2025-07-18 16:32:39 +0800

    🔄Update: change package name

--- src/main/java/com/hezhaohui/agent/AgentApplication.java
+++ src/main/java/com/hezhaohui/agent/AgentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WonderAgentApplication {
+public class AgentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WonderAgentApplication.class, args);
+        SpringApplication.run(AgentApplication.class, args);
     }
 
 }
